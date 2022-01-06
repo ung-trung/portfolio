c69e6f2acf1db6f6c0159ebd169b28e00230ee20
= <ungkientrung@gmail.com>
2022-01-06 16:55:42 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className={}>
+      <Head>
+        <title>Portfolio | Trung Ung</title>
+        <meta
+          name="description"
+          content="This is the portfolio of your next dev teammate"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Hello world
+    </div>
+  );
+}
